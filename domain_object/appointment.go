fd0d3cb1a872e84ba884923134a7b43b00e0fb29
Alireza Bashiri <azbshiri@gmail.com>
2017-11-25 13:02:39 +0330

    Change `AppointmentAttr` function parameter and return types
    Why?
    Because when using a repository there's a root collection
    which have slice of pointers to `Appointment` so higher-order
    functions (for example; WithID) should return a pointer to
    `Appointment` instead of it's value.

--- domain_object/appointment.go
+++ domain_object/appointment.go
@@ -10,33 +10,33 @@ type Appointment struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-type AppointmentAttr func(Appointment) Appointment
+type AppointmentAttr func(*Appointment) *Appointment
 
 func NewAppointment(attrs ...AppointmentAttr) *Appointment {
-	appointment := Appointment{}
+	appointment := &Appointment{}
 	for _, attr := range attrs {
 		appointment = attr(appointment)
 	}
-	return &appointment
-}
-
-var WithID = func(id int64) AppointmentAttr {
-	return func(a Appointment) Appointment {
-		a.ID = id
-		return a
-	}
+	return appointment
 }
 
 var WithCreatedAt = func(createdAt time.Time) AppointmentAttr {
-	return func(a Appointment) Appointment {
+	return func(a *Appointment) *Appointment {
 		a.CreatedAt = createdAt
 		return a
 	}
 }
 
 var WithUpdatedAt = func(updatedAt time.Time) AppointmentAttr {
-	return func(a Appointment) Appointment {
+	return func(a *Appointment) *Appointment {
 		a.UpdatedAt = updatedAt
+		return a
+	}
+}
+
+var WithID = func(id int64) AppointmentAttr {
+	return func(a *Appointment) *Appointment {
+		a.ID = id
 		return a
 	}
 }
